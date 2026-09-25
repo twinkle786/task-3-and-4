b5206d61a07c35a43813e30ca6a8b658dd9789d6
Twinkle <twinklebhardwaj7082006@gmail.com>
2026-09-25 05:54:08 +0530

    Cleanup: removed duplicate folder, fixed file naming

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 const { ApiError } = require("../middleware/errorHandler");
 
 // JWT token banane ka helper function
--- controllers/projectcontroller.js
+++ controllers/projectcontroller.js
@@ -1,4 +1,4 @@
-const Project = require("../models/Project");
+const Project = require("../models/project");
 const { ApiError } = require("../middleware/errorHandler");
 
 async function getAllProjects(req, res, next) {
